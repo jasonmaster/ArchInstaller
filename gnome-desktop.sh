f6e65e8038a75face95e452685002930ad2e54d8
Martin Wimpress <martin.wimpress@flightdataservices.com>
2012-11-27 17:16:51 +0000

    Added install of multilib-devel for 64-bit systems. Fixed Flash plugin for 64-bit systems.

--- gnome-desktop.sh
+++ gnome-desktop.sh
@@ -51,6 +51,14 @@ INSTALL_BACKUP_APPS=0
 # Configure init things
 update_early_modules ${VIDEO_KERNEL}
 
+# Install multilib-devel on x64_64. Required by other stuff.
+if [ "${CPU}" == "x86_64" ]; then
+    echo "
+Y
+Y
+Y" | sudo pacman -S --needed multilib-devel
+fi
+
 # Xorg
 pacman_install_group "xorg"
 pacman_install_group "xorg-apps"
@@ -151,7 +159,7 @@ packer_install "dropbox"
 pacman_install "nspluginwrapper flashplugin"
 #For 64-bit machines, you'll need to install
 if [ "${CPU}" == "x86_64" ]; then
-    pacman_install "lib32-flashplugin"
+    packer_install "lib32-flashplugin"
 fi
 packer_install "jre6"
 
