322e204a10153fc3fdddc237b01f2ddec8fb2ccc
Martin Wimpress <martin.wimpress@flightdataservices.com>
2012-11-28 11:09:14 +0000

    Trying to get a clean base-devel and multilib-devel

--- arch-installer.sh
+++ arch-installer.sh
@@ -288,12 +288,13 @@ elif [ "${PARTITION_LAYOUT}" == "br" ]; then
 fi
 
 # Base system
+pacstrap /mnt base base-devel openssh sudo syslinux wget
 if [ "${CPU}" == "x86_64" ]; then
-    DEVEL="multilib-devel"
-else
-    DEVEL="base-devel"
+    echo "[multilib]" >> /mnt/etc/pacman.conf
+    echo "SigLevel = PackageRequired" >> /mnt/etc/pacman.conf
+    echo "Include = /etc/pacman.d/mirrorlist" >> /mnt/etc/pacman.conf
+    pacstrap /mnt multilib-devel
 fi
-pacstrap /mnt base ${DEVEL} openssh sudo syslinux wget
 
 # Members of the 'wheel' group are sudoers
 sed -i '/%wheel ALL=(ALL) ALL/s/^#//' /mnt/etc/sudoers
