d7ccc62299dbed9b64dff9fd80c837af246b34e3
Martin Wimpress <code@flexion.org>
2013-03-02 21:57:57 +0000

    Added BCM4312 support.

---
+++ hardware/pci/14e4:4315.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -f common.sh ]; then
+    source common.sh
+else
+    echo "ERROR! Could not source 'common.sh'"
+    exit 1
+fi
+
+packer_install "b43-firmware"
+
+exit 0
