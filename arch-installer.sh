56c7501c6ee6af68a849d17b6027e424f541d1ea
Martin Wimpress <code@flexion.org>
2013-06-08 13:06:49 +0100

    Added LXDE.

--- arch-installer.sh
+++ arch-installer.sh
@@ -75,7 +75,7 @@ function usage() {
         echo "  -f : The filesystem to use. 'bfs', 'btrfs', 'ext4', 'f2fs, 'jfs', 'nilfs2', 'ntfs' and 'xfs' are supported. Defaults to '${FS}'."
     fi
     echo "  -c : The NFS export to mount and use as the pacman cache."
-    echo "  -e : The desktop environment to install. Defaults to '${DE}'. Can be 'shell', 'xorg', 'gnome', 'kde' or 'mate'."
+    echo "  -e : The desktop environment to install. Defaults to '${DE}'. Can be 'shell', 'xorg', 'gnome', 'kde', 'lxde'  or 'mate'."
     echo "  -k : The keyboard mapping to use. Defaults to '${KEYMAP}'. See '/usr/share/kbd/keymaps/' for options."
     echo "  -l : The language to use. Defaults to '${LANG}'. See '/etc/locale.gen' for options."
     echo "  -n : The hostname to use. Defaults to '${FQDN}'"
@@ -180,7 +180,7 @@ if [ "${INSTALL_TYPE}" != "desktop" ] && [ "${INSTALL_TYPE}" != "server" ] && [ 
     exit 1
 fi
 
-if [ "${DE}" != "shell" ] && [ "${DE}" != "xorg" ] && [ "${DE}" != "gnome" ] && [ "${DE}" != "kde" ] && [ "${DE}" != "mate" ] ; then
+if [ "${DE}" != "shell" ] && [ "${DE}" != "xorg" ] && [ "${DE}" != "gnome" ] && [ "${DE}" != "kde" ] && [ "${DE}" != "lxde" ] && [ "${DE}" != "mate" ] ; then
     echo "ERROR! '${DE}' is not a supported desktop environemt."
     exit 1
 fi
@@ -547,6 +547,11 @@ if [ "${INSTALL_TYPE}" == "desktop" ] && [ "${DE}" != "shell" ]; then
         add_config "systemctl enable kdm.service"
         add_config "systemctl enable upower.service"
         add_config "systemctl enable NetworkManager.service"
+        add_config "systemctl enable cups.service"
+    elif [ "${DE}" == "lxde" ]; then
+        pacstrap -c ${TARGET_PREFIX} `cat packages-xorg.txt packages-lxde.txt packages-gst.txt packages-cups.txt`
+        add_config "systemctl enable lxdm.service"
+        add_config "systemctl enable wicd.service"
         add_config "systemctl enable cups.service"
     elif [ "${DE}" == "mate" ]; then
         echo -e '\n[mate]\nServer = http://repo.mate-desktop.org/archlinux/$arch' >> ${TARGET_PREFIX}/etc/pacman.conf
