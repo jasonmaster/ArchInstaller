ff58804605044c287f9ab8b81e457bce3b8c66d0
Martin Wimpress <code@flexion.org>
2013-08-05 10:13:36 +0100

    Just found some uncommitted files.

---
+++ packages/app/faience.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+pacman -S --noconfirm --needed faenza-icon-theme faience-icon-theme faience-themes gtk-engine-unico
+
+
+
---
+++ packages/app/zukitwo.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+pacman -S --noconfirm --needed ttf-droid gtk-engine-murrine gtk-engines
+packer -S --noedit --noconfirm zukitwo-themes
+
+
+
