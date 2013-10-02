3e10d89a8fe7ee9601e1f38984cd9e50f5f6a803
Martin Wimpress <code@flexion.org>
2013-10-02 12:05:37 +0000

    Basic to test it works.

--- hardware/pci/virtualbox.sh
+++ hardware/pci/virtualbox.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
 
-exit 0
-
-# NEED IMPLEMENTING
-
-UNAME_M=`uname -m`
-KMS="radeon"
-KMS_OPTIONS="modeset=1"
-DRI="ati-dri"
-DDX="xf86-video-ati"
-DECODER="libva-vdpau-driver"
-
-pacman -S --noconfirm --needed ${DRI} ${DECODER}
-if [ "${UNAME_M}" == "x86_64" ]; then
-    pacman -S --noconfirm --needed lib32-${DRI} lib32-mesa-libgl
-fi
-echo "options ${KMS} ${KMS_OPTIONS}" > "${KMS}.conf"
+echo "*** VirtualBox ***"
+sleep 5
