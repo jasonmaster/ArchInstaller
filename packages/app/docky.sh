9d65bf978bd68447d321d4591d69b38d513b24a1
Martin Wimpress <code@flexion.org>
2013-10-09 19:47:13 +0100

    Added a new profile and new apps.

---
+++ packages/app/docky.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ `id -u` -ne 0 ]; then
+    echo "ERROR! `basename ${0}` must be executed as root."
+    exit 1
+fi
+
+CORE_PKG=$(basename ${0} .sh)
+MORE_PKGS=""
+
+pacman -S --needed --noconfirm ${CORE_PKG} ${MORE_PKGS}
---
+++ packages/app/tonto.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ `id -u` -ne 0 ]; then
+    echo "ERROR! `basename ${0}` must be executed as root."
+    exit 1
+fi
+
+CORE_PKG=$(basename ${0} .sh)
+MORE_PKGS=""
+
+IS_INSTALLED=$(pacman -Qqm ${CORE_PKG})
+if [ $? -ne 0 ]; then
+    ./jre7.sh
+    packer -S --noedit --noconfirm ${CORE_PKG} ${MORE_PKGS}
+else
+    echo "${CORE_PKG} is already installed."
+fi
