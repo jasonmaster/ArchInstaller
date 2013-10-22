c4b10edff5c22c02faa3b2a1a4153f920d17ffc8
Martin Wimpress <code@flexion.org>
2013-10-22 19:15:08 +0100

    Add TPM support to Thinkpads.

--- hardware/system/Version/ThinkPad_T43.sh
+++ hardware/system/Version/ThinkPad_T43.sh
@@ -4,7 +4,7 @@
 #  - https://communities.bmc.com/communities/blogs/linux/2010/03/16/ubuntu-1004-and-the-t43
 #  - http://pc-freak.net/blog/controlling-fan-with-thinkfan-on-lenovo-thinkpad-r61-on-debian-gnulinux-adjusting-proper-fan-cycling/
 pacman -S --noconfirm --needed hdapsd tp_smapi
-packer -S --noconfirm --noedit thinkfan hdaps-gl
+packer -S --noconfirm --noedit thinkfan hdaps-gl trousers tpm-tools tpmmanager
 echo "options thinkpad_acpi fan_control=1" > /etc/modprobe.d/thinkpad_acpi.conf
 # On the T43p the x-axis is inverted.
 echo "options hdaps invert=1" > /etc/modprobe.d/hdaps.conf
@@ -13,6 +13,7 @@ echo "options hdaps invert=1" > /etc/modprobe.d/hdaps.conf
 #  - Find a way to start `hdapsd-wrapper`
 cp /usr/share/doc/thinkfan/examples/thinkfan.conf.thinkpad /etc/thinkfan.conf
 systemctl enable thinkfan
+systemctl enable tcsd
 
 #TODO
 # - hsfmodem
